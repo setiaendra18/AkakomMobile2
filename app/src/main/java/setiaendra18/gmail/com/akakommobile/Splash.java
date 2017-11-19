d6bb90aabe02536e73b01ad33abb5e29328ccf28
Endra Setiawan <setiaendra18@gmail.com>
2017-11-20 00:44:55 +0700

    File 20/11/2017 00:44

---
+++ app/src/main/java/setiaendra18/gmail/com/akakommobile/Splash.java
@@ -0,0 +1,41 @@
+package setiaendra18.gmail.com.akakommobile;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+    public TextView tv;
+    public ImageView iv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        tv = (TextView) findViewById(R.id.tv);
+        iv = (ImageView) findViewById(R.id.iv);
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.welcome_transisi);
+        tv.startAnimation(myanim);
+        iv.startAnimation(myanim);
+        final Intent i = new Intent(this,MainActivity.class);
+        Thread timer = new Thread()
+        {
+            public void run(){
+             try {
+                 sleep(3000);
+             }
+             catch (InterruptedException e){
+                 e.printStackTrace();
+             }
+             finally {
+                 startActivity(i);
+                 finish();
+             }
+            }
+        };
+        timer.start();
+    }
+}
