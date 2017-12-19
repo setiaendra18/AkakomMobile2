ead7609c3fe4ec5c517f16203826f49c1bdc2425
mazud21 <hmazud@gmail.com>
2017-12-19 11:01:04 +0700

    update : tab_layout - adding information
    update : tautan
    - back arrow

--- app/src/main/java/setiaendra18/gmail/com/akakommobile/tautan.java
+++ app/src/main/java/setiaendra18/gmail/com/akakommobile/tautan.java
@@ -2,10 +2,11 @@ package setiaendra18.gmail.com.akakommobile;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class tautan extends AppCompatActivity  {
+public class tautan extends program_studi  {
     ListView listView;
     ArrayAdapter<CharSequence> adapter;
     @Override
@@ -15,6 +16,11 @@ public class tautan extends AppCompatActivity  {
         listView = (ListView)findViewById(R.id.listView);
         adapter = ArrayAdapter.createFromResource(this,R.array.array_list,android.R.layout.simple_list_item_1);
         listView.setAdapter(adapter);
+
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
     }
 
