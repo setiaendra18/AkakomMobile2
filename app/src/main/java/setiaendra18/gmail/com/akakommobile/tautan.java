8de369a1b2bc0b192a5b7bf554c5d2b3e4b392aa
setiaendra18 <setiaendra18@gmail.com>
2017-12-20 16:07:52 +0700

    Update News Filed , Read More Masih Force Close

--- app/src/main/java/setiaendra18/gmail/com/akakommobile/tautan.java
+++ app/src/main/java/setiaendra18/gmail/com/akakommobile/tautan.java
@@ -1,11 +1,11 @@
 package setiaendra18.gmail.com.akakommobile;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class tautan extends AppCompatActivity  {
+public class tautan extends program_studi  {
 
     ListView listView;
     ArrayAdapter<CharSequence> adapter;
@@ -17,7 +17,9 @@ public class tautan extends AppCompatActivity  {
         adapter = ArrayAdapter.createFromResource(this,R.array.array_list,android.R.layout.simple_list_item_1);
         listView.setAdapter(adapter);
 
-
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
            }
 
