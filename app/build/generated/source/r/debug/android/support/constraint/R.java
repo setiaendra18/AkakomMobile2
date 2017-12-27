e045af51f7b79011eea5dd570b05df8d59bda545
afrillaadhit <afrilla.adit@gmail.com>
2017-12-27 13:11:24 -0800

    sosial media icon added

--- app/build/generated/source/r/debug/android/support/constraint/R.java
+++ app/build/generated/source/r/debug/android/support/constraint/R.java
@@ -57,12 +57,12 @@ public final class R {
         public static final int all = 0x7f090018;
         public static final int basic = 0x7f09001d;
         public static final int chains = 0x7f090029;
-        public static final int none = 0x7f090076;
-        public static final int packed = 0x7f09007b;
-        public static final int parent = 0x7f09007d;
-        public static final int spread = 0x7f0900a5;
-        public static final int spread_inside = 0x7f0900a6;
-        public static final int wrap = 0x7f0900e4;
+        public static final int none = 0x7f09007a;
+        public static final int packed = 0x7f09007f;
+        public static final int parent = 0x7f090081;
+        public static final int spread = 0x7f0900a9;
+        public static final int spread_inside = 0x7f0900aa;
+        public static final int wrap = 0x7f0900ec;
     }
     public static final class styleable {
         public static final int[] ConstraintLayout_Layout = { 0x010100c4, 0x0101011f, 0x01010120, 0x0101013f, 0x01010140, 0x7f04005e, 0x7f0400ac, 0x7f0400ad, 0x7f0400ae, 0x7f0400af, 0x7f0400b0, 0x7f0400b1, 0x7f0400b2, 0x7f0400b3, 0x7f0400b4, 0x7f0400b5, 0x7f0400b6, 0x7f0400b7, 0x7f0400b8, 0x7f0400b9, 0x7f0400ba, 0x7f0400bb, 0x7f0400bc, 0x7f0400bd, 0x7f0400be, 0x7f0400bf, 0x7f0400c0, 0x7f0400c1, 0x7f0400c2, 0x7f0400c3, 0x7f0400c4, 0x7f0400c5, 0x7f0400c6, 0x7f0400c7, 0x7f0400c8, 0x7f0400c9, 0x7f0400ca, 0x7f0400cb, 0x7f0400cc, 0x7f0400cd, 0x7f0400cf, 0x7f0400d0, 0x7f0400d1, 0x7f0400d2, 0x7f0400d3, 0x7f0400d4, 0x7f0400d5, 0x7f0400d6, 0x7f0400d9 };
--- app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -19,7 +19,7 @@ public final class R {
         public static final int item_touch_helper_swipe_escape_velocity = 0x7f070083;
     }
     public static final class id {
-        public static final int item_touch_helper_previous_elevation = 0x7f09005c;
+        public static final int item_touch_helper_previous_elevation = 0x7f090060;
     }
     public static final class styleable {
         public static final int[] RecyclerView = { 0x010100c4, 0x010100f1, 0x7f0400a6, 0x7f04010b, 0x7f04011a, 0x7f040120 };
